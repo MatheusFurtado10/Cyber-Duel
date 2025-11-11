e874a8a633d910efabd39ad5bea601a9cdd1fb3a
Matheus Furtado <furtado.matheus@estudante.ufjf.br>
2025-11-10 22:06:08 -0300

    teste

---
+++ 202476027-202476029/src/main/java/br/ufjf/dcc/trabalho/App.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package br.ufjf.dcc.trabalho;
+import java.nio.file.Files;
+/**
+ *
+ * @author mathe
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
