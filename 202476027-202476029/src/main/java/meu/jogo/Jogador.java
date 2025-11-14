962ebe9ac2acf555f2de7c5d517df19f855c5b5d
Matheus Furtado <furtado.matheus@estudante.ufjf.br>
2025-11-14 19:36:08 -0300

    conserta a quantidade de pontos de vida que o ganhador tem

--- 202476027-202476029/src/main/java/meu/jogo/Jogador.java
+++ 202476027-202476029/src/main/java/meu/jogo/Jogador.java
@@ -218,7 +218,7 @@ public class Jogador {
                   break;
               case 3:
                   respondeu=true;
-                  jogador.vida =0;
+                  jogador.rendeu =true;
                   break; 
 
               default:
